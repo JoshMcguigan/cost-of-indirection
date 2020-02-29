2715110b46825c2122113c8f81e57c37b159536d
Josh Mcguigan <joshmcg88@gmail.com>
2020-02-29 11:07:49 -0800

    add trait object with black box test

---
+++ src/bin/trait_object_black_box.rs
@@ -0,0 +1,49 @@
+//! Note that unlike the others, this benchmark requires the nightly compiler to run.
+
+#![feature(test)]
+use std::time::Instant;
+
+fn main() {
+    let finder = OddFinderOne;
+
+    let start_time = Instant::now();
+    let result = find_nth_odd(std::hint::black_box::<&dyn OddFinder>(&finder), 1_000_000_000);
+    let elapsed_time = start_time.elapsed().as_millis();
+
+    println!("{} in {} ms", result, elapsed_time);
+}
+
+
+fn find_nth_odd(odd_finder: & dyn OddFinder, n: u64) -> u64 {
+    let mut i = 0;
+    let mut odd_count = 0;
+
+    while odd_count != n {
+        i += 1;
+        if odd_finder.is_odd(i) {
+            odd_count += 1;
+        }
+    }
+
+    i
+}
+
+trait OddFinder {
+    fn is_odd(&self, n: u64) -> bool;
+}
+
+struct OddFinderOne;
+
+impl OddFinder for OddFinderOne {
+    fn is_odd(&self, n: u64) -> bool {
+        n % 2 == 1
+    }
+}
+
+struct OddFinderTwo;
+
+impl OddFinder for OddFinderTwo {
+    fn is_odd(&self, _n: u64) -> bool {
+        unimplemented!()
+    }
+}
